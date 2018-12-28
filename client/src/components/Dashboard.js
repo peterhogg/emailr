1338078b8b1dec881ea062ec57b15df6b2bc9a9c
Peter Hoggarth <hoggarth.peter@gmail.com>
2018-12-27 23:29:32 -0500

    Added front end to view all surveys on the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import SurveyList from './Surveys/SurveyList';
 
 const Dashboard = () =>{
 	return (
 		<div>
 			<h2>Dashboard</h2>
+			<SurveyList />
 			<div className="fixed-action-btn">
-				<Link to="/surveys/new"  className="btn-floating btn-large red"><i class="material-icons">add</i></Link>
+				<Link to="/surveys/new"  className="btn-floating btn-large red"><i className="material-icons">add</i></Link>
 			</div>
 		</div>
 	);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,7 +21,6 @@ class Header extends Component{
 		}
 	}
 	render(){
-		console.log(this.props)
 		return (
 			<nav>
 				<div className="nav-wrapper">
